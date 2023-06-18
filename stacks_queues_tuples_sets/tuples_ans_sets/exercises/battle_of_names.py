b5b435779cf2efa68a1878ea70cadfaa1b91d783
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-06-18 17:02:55 +0200

    stacks queue

---
+++ stacks_queues_tuples_sets/tuples_ans_sets/exercises/battle_of_names.py
@@ -0,0 +1,15 @@
+odd_sums = set()
+even_sums = set()
+
+for count in range(1, int(input()) + 1):
+    current_sum = sum([ord(letter) for letter in input()]) // count
+
+    if current_sum % 2 == 0:
+        even_sums.add(current_sum)
+    else:
+        odd_sums.add(current_sum)
+
+if sum(odd_sums) < sum(even_sums):
+    print(*odd_sums.union(even_sums), sep=", ")
+else:
+    print(*odd_sums.difference(even_sums), sep=", ")
