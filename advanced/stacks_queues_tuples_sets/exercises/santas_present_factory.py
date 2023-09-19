d1d7f089be50c5b89f1f5c5417a964230122ca9e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-09-19 07:47:47 +0200

    Update santas_present_factory.py

--- advanced/stacks_queues_tuples_sets/exercises/santas_present_factory.py
+++ advanced/stacks_queues_tuples_sets/exercises/santas_present_factory.py
@@ -1,33 +1,46 @@
 from collections import deque
 
-materials = deque(map(int, input().split()))
+materials = list(map(int, input().split()))
 magics = deque(map(int, input().split()))
-
-result = []
 
 presents = {
     150: "Doll",
     250: "Wooden train",
     300: "Teddy bear",
-    499: "Bicycle"
+    400: "Bicycle"
 }
 
-while magics and materials:
-    current_material = materials.pop() if magics[0] or not materials[0] else 0
-    current_magic = magics.pop() if current_material or magics[0] else 0
-
-    if not current_magic:
+result = {}
+while materials and magics:
+    last_material = materials.pop() if magics[0] or not materials[0] else False
+    first_magic = magics.popleft() if last_material or not magics[0] else False
+    if not first_magic:
         continue
 
-    product = current_material * current_magic
+    magic_craft = last_material * first_magic
 
-    if presents.get(product):
-        result.append(presents[product])
+    if magic_craft in presents:
+        if presents[magic_craft] not in result:
+            result[presents[magic_craft]] = 0
+        result[presents[magic_craft]] += 1
 
-    elif product < 0:
-        materials.append(product + current_magic)
+    elif magic_craft < 0:
+        materials.append(last_material + first_magic)
 
-    elif product > 0:
-        materials.append(product + 15)
+    else:
+        materials.append(last_material + 15)
 
-print(result)
+doll_combo = True if "Doll" in result and "Wooden train" in result else False
+bear_combo = True if "Teddy bear" in result and "Bicycle" in result else False
+
+print("The presents are crafted! Merry Christmas!") if doll_combo or bear_combo \
+    else print("No presents this Christmas!")
+
+if materials:
+    print(f"Materials left: {', '.join(str(x) for x in reversed(materials))}")
+
+if magics:
+    print(f"Magic left: {', '.join(str(x) for x in magics)}")
+
+for el in sorted(result):
+    print(f"{el}: {result[el]}")
