b281d60b199ab7b872fa1f8e4093f00b4d175dd1
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-09-19 23:55:54 +0200

    Update paint_colors.py

--- advanced/stacks_queues_tuples_sets/exercises/paint_colors.py
+++ advanced/stacks_queues_tuples_sets/exercises/paint_colors.py
@@ -1,31 +1,37 @@
 from collections import deque
 
-words = deque(input().split())
+input_colors = deque(input().split())
 
-colors = {
-    "orange": {"yellow", "red"},
-    "green": {"yellow", "blue"},
-    "purple": {"red", "blue"}
-}
+main_colors = ["red", "yellow", "blue"]
+secondary_colors = {"orange": ["red", "yellow"],
+                    "purple": ["red", "blue"],
+                    "green": ["yellow", "blue"]}
+
+found_colors = []
+
+while input_colors:
+    first_color = input_colors.popleft()
+    second_color = input_colors.pop() if input_colors else ""
+    current_color = [x for x in [first_color + second_color, second_color + first_color]
+                     if x in main_colors or x in secondary_colors]
+    if current_color:
+        found_colors.append(current_color[0])
+    else:
+        first_color = first_color[:len(first_color) - 1]
+        second_color = second_color[:len(second_color) - 1]
+        if first_color:
+            input_colors.insert(len(input_colors) // 2, first_color)
+        if second_color:
+            input_colors.insert(len(input_colors) // 2, second_color)
 
 result = []
 
-while words:
-    first_word = words.popleft()
-    second_word = words.pop() if words else ""
-
-    for color in (first_word + second_word, second_word + first_word):
-        if color in colors:
-            result.append(color)
-            break
-
+for color in found_colors:
+    if color in main_colors:
+        result.append(color)
     else:
-        for el in (first_word[:-1], second_word[:-1]):
-            if el:
-                words.insert(len(words) // 2, el)
-
-for color in set(colors.keys()).intersection(result):
-    if not colors[color].issubset(result):
-        result.remove(color)
+        if all(c in found_colors for c in secondary_colors[color]):
+            result.append(color)
 
 print(result)
+
