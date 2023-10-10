055177eceb9b9165fb2edac8f34590065f90ce64
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-10-10 08:08:37 +0200

    vehicles

---
+++ oop/exams/retake_exam_18_april_2023/project/vehicles/passenger_car.py
@@ -0,0 +1,14 @@
+from project.vehicles.base_vehicle import BaseVehicle
+
+
+class PassengerCar(BaseVehicle):
+    MAX_MILEAGE = 450.00
+    TYPE = "PassengerCar"
+    def __init__(self, brand: str, model: str, license_plate_number: str):
+        super().__init__(brand, model, license_plate_number, max_mileage=self.MAX_MILEAGE)
+
+    def drive(self, mileage):
+        self.battery_level -= round(mileage / self.MAX_MILEAGE * 100)
+
+
+
